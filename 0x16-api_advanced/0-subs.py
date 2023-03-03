766cc64c0c8692185d8ccaae7a1cb6a90376fc50
Kaditcuy <philipukchimaobi@gmail.com>
2023-03-03 06:30:38 +0000

    fetched subreddit(programmers) subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Script that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        return json_data["data"]["subscribers"]
+    else:
+        return 0
