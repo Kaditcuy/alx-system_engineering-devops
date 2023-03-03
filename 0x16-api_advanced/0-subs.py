34997b2c9209970a665d7aac5a53d0c1022bec09
Kaditcuy <philipukchimaobi@gmail.com>
2023-03-03 06:39:23 +0000

    Fixed pep8 issue

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 """
-Script that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
+Script that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """Fetches a subreddits number of subscribers"""
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
